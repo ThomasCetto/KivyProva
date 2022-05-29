5671a7a27b2f6d76ab8c5acce96a782937ef5b32
Thomas Cetto <64642352+ThomasCetto@users.noreply.github.com>
2022-05-29 12:57:41 +0200

    Add files via upload

---
+++ Bacteria/main.py
@@ -0,0 +1,25 @@
+import kivy
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+import random
+
+kivy.require('2.1.0')
+
+#
+
+class MyRoot(BoxLayout):
+
+    def __init__(self):
+        super(MyRoot, self).__init__()
+
+    def generate_number(self):
+        self.randomLabel.text = str(random.randint(0, 1000))
+
+class AppDiProva(App):
+
+    def build(self):
+        return MyRoot()
+
+prova = AppDiProva()
+prova.run()
